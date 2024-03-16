09be5c97d4191f288797a0177b4070870da2192b
Tom Van Dort <tom@vandort.dev>
2024-03-15 23:06:12 -0400

    Attempt to access static method?

--- common/overrides.d.ts
+++ common/overrides.d.ts
@@ -7,6 +7,7 @@ declare class MeasuredTemplateOverride extends MeasuredTemplate {
 	hover: typeof MeasuredTemplate.prototype._hover;
 	highlightId: string;
 	document: (typeof MeasureTemplate)["document"] & { user: { id: string } };
+	static getRectShape(direction: number, distance: number, mystery: boolean): Rectangle;
 }
 
 declare namespace PIXI {
