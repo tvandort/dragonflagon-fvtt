aaaa7cccde048fb48eaeb1c77f8f17d022cdedf0
Tom Van Dort <tom@vandort.dev>
2024-03-15 23:08:37 -0400

    Hmm

--- common/overrides.d.ts
+++ common/overrides.d.ts
@@ -7,7 +7,7 @@ declare class MeasuredTemplateOverride extends MeasuredTemplate {
 	hover: typeof MeasuredTemplate.prototype._hover;
 	highlightId: string;
 	document: (typeof MeasureTemplate)["document"] & { user: { id: string } };
-	static getRectShape(direction: number, distance: number, mystery: boolean): Rectangle;
+	static getRectShape(direction: number, distance: number, mystery: boolean): PIXI.Rectangle;
 }
 
 declare namespace PIXI {
