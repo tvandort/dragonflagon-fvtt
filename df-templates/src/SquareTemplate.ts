f4a14bd60276f2fd2c12e697a0197b14e228cbe5
Tom Van Dort <tom@vandort.dev>
2024-03-15 21:15:21 -0400

    Update _getRectShape to getRectShape.
    Thanks to: https://github.com/flamewave000/dragonflagon-fvtt/issues/481#issuecomment-1598669028

--- df-templates/src/SquareTemplate.ts
+++ df-templates/src/SquareTemplate.ts
@@ -1,35 +1,61 @@
 import SETTINGS from "../../common/Settings";
 
 export default class SquareTemplate {
-	static readonly FIX_ROTATION_PREF = 'fix-square-rotation';
+	static readonly FIX_ROTATION_PREF = "fix-square-rotation";
 
 	static init() {
 		SETTINGS.register(SquareTemplate.FIX_ROTATION_PREF, {
 			config: true,
-			scope: 'world',
-			name: 'DF_TEMPLATES.SquareRotateName',
-			hint: 'DF_TEMPLATES.SquareRotateHint',
+			scope: "world",
+			name: "DF_TEMPLATES.SquareRotateName",
+			hint: "DF_TEMPLATES.SquareRotateHint",
 			type: Boolean,
 			default: true,
-			onChange: (toggled) => toggled ? SquareTemplate.patch() : SquareTemplate.unpatch()
+			onChange: (toggled) =>
+				toggled ? SquareTemplate.patch() : SquareTemplate.unpatch(),
 		});
 		if (SETTINGS.get(SquareTemplate.FIX_ROTATION_PREF))
 			SquareTemplate.patch();
 	}
 
 	private static patch() {
-		libWrapper.register(SETTINGS.MOD_NAME, 'MeasuredTemplate.prototype._getRectShape', SquareTemplate.MeasuredTemplate_getRectShape, 'OVERRIDE');
-		libWrapper.register(SETTINGS.MOD_NAME, 'MeasuredTemplate.prototype._refreshRulerText', SquareTemplate.MeasuredTemplate_refreshRulerText, 'WRAPPER');
+		libWrapper.register(
+			SETTINGS.MOD_NAME,
+			"MeasuredTemplate.prototype.getRectShape",
+			SquareTemplate.MeasuredTemplate_getRectShape,
+			"OVERRIDE"
+		);
+		libWrapper.register(
+			SETTINGS.MOD_NAME,
+			"MeasuredTemplate.prototype._refreshRulerText",
+			SquareTemplate.MeasuredTemplate_refreshRulerText,
+			"WRAPPER"
+		);
 	}
 	private static unpatch() {
-		libWrapper.unregister(SETTINGS.MOD_NAME, 'MeasuredTemplate.prototype._getRectShape', false);
-		libWrapper.unregister(SETTINGS.MOD_NAME, 'MeasuredTemplate.prototype._refreshRulerText', false);
+		libWrapper.unregister(
+			SETTINGS.MOD_NAME,
+			"MeasuredTemplate.prototype.getRectShape",
+			false
+		);
+		libWrapper.unregister(
+			SETTINGS.MOD_NAME,
+			"MeasuredTemplate.prototype._refreshRulerText",
+			false
+		);
 	}
 
-	static MeasuredTemplate_getRectShape(this: MeasuredTemplate, direction: number, distance: number, adjustForRoundingError = false): PIXI.Polygon {
+	static MeasuredTemplate_getRectShape(
+		this: MeasuredTemplate,
+		direction: number,
+		distance: number,
+		adjustForRoundingError = false
+	): PIXI.Polygon {
 		// Generate a rotation matrix to apply the rect against. The base rotation must be rotated
 		// CCW by 45° before applying the real direction rotation.
-		const matrix = PIXI.Matrix.IDENTITY.rotate((-45 * (Math.PI / 180)) + direction);
+		const matrix = PIXI.Matrix.IDENTITY.rotate(
+			-45 * (Math.PI / 180) + direction
+		);
 		// If the shape will be used for collision, shrink the rectangle by a fixed EPSILON amount to account for rounding errors
 		const EPSILON = adjustForRoundingError ? 0.0001 : 0;
 		// Use simple Pythagoras to calculate the square's size from the diagonal "distance".
@@ -41,7 +67,20 @@ export default class SquareTemplate {
 		const botLeft = matrix.apply(new PIXI.Point(EPSILON, size));
 		const botRight = matrix.apply(new PIXI.Point(size, size));
 		// Inject the vector data into a Polygon object to create a closed shape.
-		const shape = <any>new PIXI.Polygon([topLeft.x, topLeft.y, topRight.x, topRight.y, botRight.x, botRight.y, botLeft.x, botLeft.y, topLeft.x, topLeft.y]);
+		const shape = <any>(
+			new PIXI.Polygon([
+				topLeft.x,
+				topLeft.y,
+				topRight.x,
+				topRight.y,
+				botRight.x,
+				botRight.y,
+				botLeft.x,
+				botLeft.y,
+				topLeft.x,
+				topLeft.y,
+			])
+		);
 		// Add these fields so that the Sequencer mod doesn't have a stroke lol
 		shape.x = topLeft.x;
 		shape.y = topLeft.y;
@@ -50,12 +89,17 @@ export default class SquareTemplate {
 		return <PIXI.Polygon>shape;
 	}
 
-	private static MeasuredTemplate_refreshRulerText(this: MeasuredTemplate, wrapped: () => void): void {
+	private static MeasuredTemplate_refreshRulerText(
+		this: MeasuredTemplate,
+		wrapped: () => void
+	): void {
 		wrapped();
 		// Overwrite the text for the "rect" type
 		if (this.document.t === "rect") {
 			// Use simple Pythagoras to calculate the square's size from the diagonal "distance".
-			const size = Math.sqrt((this.document.distance * this.document.distance) / 2).toFixed(1);
+			const size = Math.sqrt(
+				(this.document.distance * this.document.distance) / 2
+			).toFixed(1);
 			const text = `${size}${canvas.scene.grid.units}`;
 			(<any>this).ruler.text = text;
 		}
